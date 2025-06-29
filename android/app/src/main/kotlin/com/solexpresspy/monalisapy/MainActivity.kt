a5a11d2d5615e8ce79b3c81ae10ce43f1b6d6d4f
jumbo <jumbointl@gmail.com>
2025-06-29 18:06:58 -0300

    2025-06-29-20:00

---
+++ android/app/src/main/kotlin/com/solexpresspy/monalisapy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.solexpresspy.monalisapy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/products/domain/idempiere/idempiere_storage_on_hande.dart
@@ -0,0 +1,137 @@
+import 'package:monalisa_app_001/features/products/domain/idempiere/idempiere_warehouse.dart';
+
+import 'idempiere_locator.dart';
+import 'idempiere_object.dart';
+import 'idempiere_organization.dart';
+import 'idempiere_product.dart';
+import 'idempiere_tenant.dart';
+import 'idempiere_user.dart';
+
+import 'idempiere_attribute_set_instance.dart';
+
+class IdempiereStorageOnHande extends IdempiereObject{
+  String? uid;
+  IdempiereTenant? aDClientID;
+  IdempiereOrganization? aDOrgID;
+  String? created;
+  IdempiereUser? createdBy;
+  bool? isActive;
+  IdempiereAttributeSetInstance? mAttributeSetInstanceID;
+  IdempiereLocator? mLocatorID;
+  IdempiereProduct? mProductID;
+  double? qtyOnHand;
+  String? updated;
+  IdempiereUser? updatedBy;
+  String? dateMaterialPolicy;
+  IdempiereWarehouse? warehouse;
+
+  IdempiereStorageOnHande(
+      { 
+        
+        this.uid,
+        this.aDClientID,
+        this.aDOrgID,
+        this.created,
+        this.createdBy,
+        this.isActive,
+        this.mAttributeSetInstanceID,
+        this.mLocatorID,
+        this.mProductID,
+        this.qtyOnHand,
+        this.updated,
+        this.updatedBy,
+        this.dateMaterialPolicy,
+        this.warehouse,
+        super.modelName,
+        super.id,
+        super.identifier,
+        super.propertyLabel,
+        super.active,
+        super.category,
+        super.name,
+        super.image,
+      });
+
+  IdempiereStorageOnHande.fromJson(Map<String, dynamic> json) {
+    uid = json['uid'];
+    aDClientID = json['AD_Client_ID'] != null
+        ?  IdempiereTenant.fromJson(json['AD_Client_ID'])
+        : null;
+    aDOrgID = json['AD_Org_ID'] != null
+        ?  IdempiereOrganization.fromJson(json['AD_Org_ID'])
+        : null;
+    created = json['Created'];
+    createdBy = json['CreatedBy'] != null
+        ?  IdempiereUser.fromJson(json['CreatedBy'])
+        : null;
+    isActive = json['IsActive'];
+    mAttributeSetInstanceID = json['M_AttributeSetInstance_ID'] != null
+        ?  IdempiereAttributeSetInstance.fromJson(
+        json['M_AttributeSetInstance_ID'])
+        : null;
+    mLocatorID = json['M_Locator_ID'] != null
+        ?  IdempiereLocator.fromJson(json['M_Locator_ID'])
+        : null;
+    mProductID = json['M_Product_ID'] != null
+        ?  IdempiereProduct.fromJson(json['M_Product_ID'])
+        : null;
+    qtyOnHand = json['QtyOnHand'] != null ? double.tryParse(json['QtyOnHand'].toString()) : null;
+    updated = json['Updated'];
+    updatedBy = json['UpdatedBy'] != null
+        ?  IdempiereUser.fromJson(json['UpdatedBy'])
+        : null;
+    dateMaterialPolicy = json['DateMaterialPolicy'];
+    modelName = json['model-name'];
+    id = json['id'];
+    identifier = json['identifier'];
+    propertyLabel = json['propertyLabel'];
+    active = json['active'];
+    category = json['category'];
+    name = json['name'];
+    image = json['image'];
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data =  <String, dynamic>{};
+    data['uid'] = uid;
+    if (aDClientID != null) {
+      data['AD_Client_ID'] = aDClientID!.toJson();
+    }
+    if (aDOrgID != null) {
+      data['AD_Org_ID'] = aDOrgID!.toJson();
+    }
+    data['Created'] = created;
+    if (createdBy != null) {
+      data['CreatedBy'] = createdBy!.toJson();
+    }
+    data['IsActive'] = isActive;
+    if (mAttributeSetInstanceID != null) {
+      data['M_AttributeSetInstance_ID'] =
+          mAttributeSetInstanceID!.toJson();
+    }
+    if (mLocatorID != null) {
+      data['M_Locator_ID'] = mLocatorID!.toJson();
+    }
+    if (mProductID != null) {
+      data['M_Product_ID'] = mProductID!.toJson();
+    }
+    data['QtyOnHand'] = qtyOnHand;
+    data['Updated'] = updated;
+    if (updatedBy != null) {
+      data['UpdatedBy'] = updatedBy!.toJson();
+    }
+    data['DateMaterialPolicy'] = dateMaterialPolicy;
+    data['model-name'] = modelName;
+    data['id'] = id;
+    data['identifier'] = identifier;
+    data['propertyLabel'] = propertyLabel;
+    data['active'] = active;
+    data['category'] = category;
+    data['name'] = name;
+    data['image'] = image;
+    
+    return data;
+  }
+}
+
